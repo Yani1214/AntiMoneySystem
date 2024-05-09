ab76ae37cc25d5055a72a583b279af7988911aa6
Chuifengdinglv <l18278045823@icloud.com>
2024-05-09 13:54:21 +0800

    1.所有页面权限开放 2.登陆注册功能实现 3.数据库更新

--- ADMIN/src/layout/components/zy-header/header-user.vue
+++ ADMIN/src/layout/components/zy-header/header-user.vue
@@ -2,16 +2,16 @@
   <div class="header-user">
     <a-dropdown v-if="userInfo.nickname" class="header-user-dropdown">
       <div class="header-user-logo">
-        <ZyLogo size="25" :url="userInfo.avatar" :showTitle="false"/>
+        <ZyLogo size="25" :url="userInfo.avatar" :showTitle="false" />
         <span class="header-user-name">
-           {{ userInfo.nickname }}
+          {{ userInfo.nickname }}
         </span>
       </div>
       <template #overlay>
         <a-menu>
           <a-menu-item @click="logout">
             <span>
-              <IconFont type="icon-guanbi"/>
+              <IconFont type="icon-guanbi" />
               退出系统
             </span>
           </a-menu-item>
@@ -23,62 +23,58 @@
 </template>
 
 <script setup>
-import {
-  UserOutlined,
-  ExclamationCircleOutlined
-} from '@ant-design/icons-vue';
-import {Modal} from 'ant-design-vue';
-import {createVNode} from 'vue';
-import {useRouter} from 'vue-router';
-import {useAuthStore} from '../../../stores/auth.js';
-import dbUtils from '../../../libs/util.strotage.js'
-import ZyLogo from "comps/common/ZyLogo.vue";
+  import { UserOutlined, ExclamationCircleOutlined } from "@ant-design/icons-vue";
+  import { Modal } from "ant-design-vue";
+  import { createVNode } from "vue";
+  import { useRouter } from "vue-router";
+  import { useAuthStore } from "../../../stores/auth.js";
+  import dbUtils from "../../../libs/util.strotage.js";
+  import ZyLogo from "comps/common/ZyLogo.vue";
 
-const router = useRouter()
-const authStore = useAuthStore()
-const userInfo = dbUtils.get('userInfo')
-const logout = () => {
-  Modal.confirm({
-    title: '确认退出系统?',
-    icon: createVNode(ExclamationCircleOutlined),
-    onOk() {
-      authStore.logout()
-    },
-  });
-}
-const noLoginHandel = () => {
-  authStore.logout()
-}
-
+  const router = useRouter();
+  const authStore = useAuthStore();
+  const userInfo = dbUtils.get("userInfo") || {
+    nickname: "",
+    avatar: ""
+  };
+  const logout = () => {
+    Modal.confirm({
+      title: "确认退出系统?",
+      icon: createVNode(ExclamationCircleOutlined),
+      onOk() {
+        authStore.logout();
+      }
+    });
+  };
+  const noLoginHandel = () => {
+    authStore.logout();
+  };
 </script>
 
 <style lang="scss" scoped>
-.header-user {
-  cursor: pointer;
-  transition: all .2s linear;
-  box-sizing: border-box;
-  padding: 0 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-
-  .header-user-logo {
+  .header-user {
+    cursor: pointer;
+    transition: all 0.2s linear;
+    box-sizing: border-box;
+    padding: 0 8px;
     display: flex;
     justify-content: center;
     align-items: center;
 
-    .header-user-name {
-      margin-left: 8px;
-      transition: all .2s linear;
-      font-size: .9rem;
+    .header-user-logo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-      &:hover {
-        color: $color-primary;
+      .header-user-name {
+        margin-left: 8px;
+        transition: all 0.2s linear;
+        font-size: 0.9rem;
+
+        &:hover {
+          color: $color-primary;
+        }
       }
     }
   }
-
-
-}
 </style>
